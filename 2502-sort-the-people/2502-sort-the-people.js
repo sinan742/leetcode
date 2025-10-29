dedd0f250a73f36ea27d7a0634466803f3fa3649
Muhammed Sinan A <monusinan4567@gmail.com>
2025-10-29 12:32:27 +0530

    Time: 6 ms (83.47%), Space: 58.5 MB (83.47%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+  return names
+  .map((name,i)=>[name,heights[i]])
+  .sort((a,b)=>b[1] - a[1])
+  .map((person)=>person[0])
+
+};
