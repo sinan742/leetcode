10c5107077cdaa87ba0d516a9f7c832c7cbfb3a0
Muhammed Sinan A <monusinan4567@gmail.com>
2025-10-30 16:19:08 +0530

    Time: 1 ms (42.54%), Space: 55.9 MB (31.13%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let res = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % m === 0) res -= i;   
+    else res += i;               
+  }
+  return res;
+};
