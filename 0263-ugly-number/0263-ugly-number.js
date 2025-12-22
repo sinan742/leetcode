3eec29a2c29ae602d665d5f9fdf87af06785fd73
Muhammed Sinan A <monusinan4567@gmail.com>
2025-12-22 10:18:10 +0530

    Time: 1 ms (33.27%), Space: 55.3 MB (81.59%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+
+    if (n<=0) return false;
+
+    const factors = [2,3,5]
+    for(let factor of factors){
+        while(n % factor === 0){
+
+        n = n/ factor}
+    }
+
+    return n===1
+
+
+}
