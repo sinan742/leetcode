971e3362cf8a1c37d3f39ad005303ea8c12282f1
Muhammed Sinan A <monusinan4567@gmail.com>
2026-01-09 10:02:51 +0530

    Time: 3 ms (67.88%), Space: 12.7 MB (18.38%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        first =int(num1)
+        second=int(num2)
+        
+        return str( first + second )
+
+            
+
+        return res
+        
