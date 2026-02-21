b2f05589b82a169993115b172855244e27d716ba
Muhammed Sinan A <monusinan4567@gmail.com>
2026-02-21 10:22:51 +0530

    Time: 0 ms (100%), Space: 12.5 MB (22.07%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        res = int(num1) * int(num2)
+        return str(res)
+        
